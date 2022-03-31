f61f807f344907f3346e8918173d6bf30580451a
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-03-31 11:14:26 +0200

    ajout des images

---
+++ app.js
@@ -0,0 +1,51 @@
+const cardArray = [
+    {
+        name: 'a tiger',
+        image: 'images/tiger.jpg'
+    },
+    {
+        name: '2 cows',
+        image: 'images/2-cows.jpg'
+    },
+    {
+        name: 'a blue paon',
+        image: 'images/blue-paon.jpg'
+    },
+    {
+        name: 'a cerf',
+        image: 'images/cerf.jpg'
+    },
+    {
+        name: 'a girafe',
+        image: 'images/girafe.jpg'
+    },
+    {
+        name: 'jellyfish',
+        image: 'images/jellyfish.jpg'
+    },
+    {
+        name: 'A lion',
+        image: 'images/lion.jpg'
+    },
+    {
+        name: 'an ape',
+        image: 'images/monkey.jpg'
+    },
+    {
+        name: 'blank',
+        image: 'images/moon.jpg'
+    },
+    {
+        name: '2 owls',
+        image: 'images/owls.jpg'
+    },
+    {
+        name: 'a panther',
+        image: 'images/panther.jpg'
+    },
+    {
+        name: 'a raycoon',
+        image: 'images/raycoon.jpg'
+    }
+
+]
